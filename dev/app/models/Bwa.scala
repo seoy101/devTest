f060bf0ce438c0243ac41bef6932bafeba3ee397
yuhadam <yuhadam@naver.com>
2016-08-09 14:07:32 +0900

    aaaa

--- dev/app/models/Bwa.scala
+++ dev/app/models/Bwa.scala
@@ -5,7 +5,7 @@ case class Bwa (exe_name: String , option1: String)
 
 object Bwa{
   def getCmd(bwa:Bwa):String={
-    val cmd = "./"+bwa.exe_name+" "+bwa.option1
+    val cmd = "./"+bwa.exe_name+" "+bwa.option1+" "
     return cmd
   }
 }
