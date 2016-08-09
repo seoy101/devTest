1daaa8664ad7a04c172b5282e9c89d07e42bd00c
yuhadam <yuhadam@naver.com>
2016-08-09 16:25:26 +0900

    test

--- launch.sh
+++ launch.sh
@@ -31,6 +31,3 @@ echo  '"uris": [],' >> $JOBPATH/docker.json
 echo  '"command":' "\"$JOBPATH/innerSh.sh\"" >> $JOBPATH/docker.json
 echo '}' >> $JOBPATH/docker.json
 cd $JOBPATH
-sudo curl -L -H 'Content-Type: application/json' -X POST -d @docker.json 10.32.21.89:30842/scheduler/iso8601
-
-sudo curl -L -X PUT 10.32.21.89:30842/scheduler/job/$JOB_NAME
