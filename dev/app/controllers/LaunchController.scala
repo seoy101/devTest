ba5936b269cef4d927cb2663f10d0412ab892a00
root <root@mail.google.com>
2016-08-18 10:49:51 +0900

    aaa

--- dev/app/controllers/LaunchController.scala
+++ dev/app/controllers/LaunchController.scala
@@ -13,7 +13,77 @@ import java.util.Date
 import java.io._
 import java.text.SimpleDateFormat
 
-class LaunchController @Inject() (val messagesApi:MessagesApi) extends Controller with I18nSupport  {
+
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test._
+import java.io.{FileWriter, FileOutputStream, File}
+
+import controllers._
+import play.api.libs.Files.TemporaryFile
+import play.api.mvc.MultipartFormData.FilePart
+
+import java.io.File
+
+import java.nio.file.attribute.PosixFilePermission._
+import java.nio.file.attribute.PosixFilePermissions
+import java.nio.file.{Files, Path}
+import java.nio.file.Paths
+import java.util
+import javax.inject._
+
+import akka.stream.IOResult
+import akka.stream.scaladsl._
+import akka.util.ByteString
+import akka.actor._
+import akka.stream.Materializer
+
+
+
+import play.api.libs.streams._
+import play.api.mvc.MultipartFormData.FilePart
+
+import play.api.libs.streams.Accumulator
+
+import play.api.mvc.BodyParsers.parse
+
+import play.api.mvc.BodyParsers.parse._
+import play.core.parsers.Multipart.FileInfo
+import scala.concurrent.Future
+
+import java.io.ByteArrayOutputStream
+
+import play.api.libs.iteratee.Iteratee
+
+import play.api.mvc.BodyParsers.parse._
+
+import play.api.mvc.{BodyParser, MultipartFormData}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import play.core.parsers.Multipart
+
+import play.api.mvc.BodyParsers.parse.multipartFormData
+
+import play.api.libs.streams._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import akka.util.ByteString
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.actor._
+import akka.stream.Materializer
+import play.api.mvc.Result
+
+import java.nio.file.attribute.BasicFileAttributes
+import akka.stream.scaladsl.{FileIO, Sink}
+import akka.stream.scaladsl.FileIO
+import java.nio.file.StandardOpenOption
+
+import java.nio.file.StandardOpenOption
+import akka.stream.SinkShape
+import akka.NotUsed
+import reflect.io._
+
+class LaunchController @Inject() (implicit system: ActorSystem, materializer: Materializer, val messagesApi:MessagesApi) extends Controller with I18nSupport  {
     
   private val BwaForm : Form[Bwa]=Form(mapping(                        
                                                 "exe_name" -> nonEmptyText,
@@ -23,13 +93,14 @@ class LaunchController @Inject() (val messagesApi:MessagesApi) extends Controlle
   
   def index= Action{
     implicit request =>
-      Ok(views.html.index())
+      Ok(views.html.bwa())
   }
   
   def analysis = Action{
      implicit request =>
        Ok(views.html.bwa())
   }
+  
   def launch =Action{
     implicit request =>
       val bwa=BwaForm.bindFromRequest()
@@ -62,6 +133,32 @@ class LaunchController @Inject() (val messagesApi:MessagesApi) extends Controlle
            }
            )
   }  
+  //////////////////
+def fileupload =Action{
+  implicit request =>
+    Ok(views.html.fileupload())
+}
+  type FilePartHandler[A] = FileInfo => Accumulator[ByteString, FilePart[A]]
+
+  def handleFilePartAsFile: FilePartHandler[File] = {
+        case FileInfo(partName, filename, contentType) =>
+          val filepath = Paths.get("/nfsdir/test")   
+         val filesink :Sink[ByteString, Future[IOResult]] =FileIO.toPath(filepath,Set(StandardOpenOption.CREATE_NEW,StandardOpenOption.WRITE))        
+         val accumulator = Accumulator(filesink)        
+         accumulator.map { case IOResult(count, status) =>                    
+          FilePart(partName, filename, contentType, filepath.toFile())
+        }(play.api.libs.concurrent.Execution.defaultContext)
+        } 
+
+def uploadCustom = Action(parse.multipartFormData(handleFilePartAsFile,1000000000000L)) { request =>
+  val fileOption = request.body.file("name").map {
+    case FilePart(key, filename, contentType, file) =>
+      file.toPath
+  }
+
+  Ok(s"File uploaded: $fileOption")
+}
+//////////////////////
   def sftpresult(data:String) = Action{
     implicit request =>
    
